f8c3c86e957a2199253b4cf8e977d78bbfce7d61
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-14 23:50:45 +0900

    Initial Commit

---
+++ SearchZIipcode/SearchZIipcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchZIipcodeApp.swift
+//  SearchZIipcode
+//
+//  Created by 田中大地 on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct SearchZIipcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
